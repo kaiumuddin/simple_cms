4303f58b0874f220fb004feb38ecae123b0bc5ea
kaiumuddin <kaium.uddin2909@gmail.com>
2024-05-26 11:41:14 +0600

    Added form options helper for views and controller.

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -13,6 +13,8 @@ class PagesController < ApplicationController
 
   def new
     @page = Page.new
+    @page_count = Page.count + 1
+    @subjects = Subject.sorted
   end
 
   def create
@@ -22,12 +24,16 @@ class PagesController < ApplicationController
       flash[:notice] = 'Page created successfully'
       redirect_to(pages_path)
     else
+      @page_count = Page.count + 1
+      @subjects = Subject.sorted
       render 'new'
     end
   end
 
   def edit
     @page = Page.find(params[:id])
+    @page_count = Page.count
+    @subjects = Subject.sorted
   end
 
   def update
@@ -37,6 +43,8 @@ class PagesController < ApplicationController
       flash[:notice] = 'Page updated successfully'
       redirect_to(page_path(@page))
     else
+      @page_count = Page.count
+      @subjects = Subject.sorted
       render('edit')
     end
   end
